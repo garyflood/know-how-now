33ac80be1c5fdc08213e4951b1929284f5e32e31
Gary Flood <45420768+garyflood@users.noreply.github.com>
2026-03-11 16:22:51 +0100

    change redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(_resource)
+    profile_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    profile_path
+  end
+
   private
 
   # Allow extra fields on Devise sign up (full_name, username)
