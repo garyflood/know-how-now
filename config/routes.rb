5e74b1fe59dc6cb8b4259d13af58e13d5ac50459
Emmanuel Nwabufo <fremmanuel@shanahanef.org>
2026-03-11 14:17:04 +0100

    feature/edit-user-profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :devices, only: [:create, :show]
   get "explore", to: "pages#explore"
   get "profile", to: "users#show", as: :profile
+  get "profile/edit", to: "users#edit", as: :edit_profile
+  patch "profile", to: "users#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
