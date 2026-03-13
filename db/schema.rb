37189c98bb0a103f8d939d7cbf3217a66ac0f9a3
Emmanuel Nwabufo <fremmanuel@shanahanef.org>
2026-03-13 12:54:40 +0100

    Add related devices page with search and list layout

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_13_120000) do
   create_table "videos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.bigint "device_id", null: false
-    t.string "source"
     t.string "summary", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
